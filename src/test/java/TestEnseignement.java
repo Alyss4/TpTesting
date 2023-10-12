3b59433f03ca016a06681c6dad20d4c90116e140
Alyss4 <alyssafc8@gmail.com>
2023-10-12 12:48:43 +0200

    algo

--- src/test/java/TestEnseignement.java
+++ src/test/java/TestEnseignement.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
 public class TestEnseignement {
+    @Test
+    public void testAjoutEvaluation(){
+        //Instancier un Enseignement
+        //Creer une Evaluation
+        //Appeler methode ajouterEvaluation
+        //Recuperer Arraylist enseignement
+        //Verifier que evaluation ajouter à enseignement
+    }
 }
--- src/test/java/TestProf.java
+++ src/test/java/TestProf.java
@@ -1,2 +1,14 @@
+import org.junit.Test;
+
 public class TestProf {
+    @Test
+    public void testAjoutEnseignement(){
+        //Instancier un Prof
+        //Instancier Enseignement
+        //Créer enseignement
+        //Appeler méthode ajouter enseignement sur le prof créer
+        //Récupérer Array lsit des enseignement du prof
+        //Vérifier que enseignement est ajouté à professeur
+
+    }
 }
